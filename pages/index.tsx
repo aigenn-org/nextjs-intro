05922a88ccb045d8fc6bc22035d26f9931c648e4
dev <dev@aigenn.com>
2023-01-26 16:22:14 +0900

    fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,12 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState()
-  useEffect(() => {
-    (async() => {
-      const {results} = await(
-        await fetch(`/api/movies`)).json();
-      setMovies(results)
-    });
-  }, []);
+export default function Home({results}: any) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
+      {!results && <h4>Loading...</h4>}
+      {results?.map((movie: any) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -45,4 +37,14 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+// only run in server
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
